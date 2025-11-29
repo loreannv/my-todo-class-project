3f0954049d5c0281314cdc72b08a416e4a12cd3a
jesus2801 <jesusd280147\@gmail.com>
2025-11-29 15:23:27 -0500

    Sentry implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://5ee4e6a1fdcce1466d8eb4dfa17295c8@o4510450093981696.ingest.us.sentry.io/4510450097717248",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 createRoot(document.getElementById('root')!).render(
   <App />
--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,12 +1,19 @@
 import Login from "../../components/login";
 
 const Public = () => {
-    return (
-        <>
-            <h1>Public</h1>
-            <Login />
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Public</h1>
+      <button
+        onClick={() => {
+          throw new Error("This is your first error!");
+        }}
+      >
+        Break the world
+      </button>
+      <Login />
+    </>
+  );
+};
 
 export default Public;
